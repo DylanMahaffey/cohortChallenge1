718225c8ea97a225152163ded3a69af851b5f5d2
Dylan Mahaffey <dylan.mahaffey@gmail.com>
2019-08-23 23:18:25 -0700

    everything but the mobile friendly because I got ahead of myself before creating this repo

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+$( document ).ready(function() {
+    // Chat window functionality (even though it doesn't work yet)
+    $("#chat").click(e => {
+        $("#chat-window").removeClass("hide-chat");
+    })
+    $("#close").click(e => {
+        $("#chat-window").addClass("hide-chat");
+    })
+    $("#chat-send").click(e => {
+        const message = `<span class="bubble sent">${$("#chat-body").val()}</span>`
+        $("#convo-display").append(message);
+
+        $("#chat-body").val('')
+    })
+
+
+    // Scroll Animations
+    $(".hidden-load").removeClass('hidden-load');
+
+    $("#ui-ux-p").viewportChecker({
+        classToRemove: 'hidden'
+    })
+    $("#conversion").viewportChecker({
+        classToRemove: 'hidden'
+    })
+    $("#ui-left").viewportChecker({
+        classToRemove: 'hidden-left'
+    })
+    $("#ux-right").viewportChecker({
+        classToRemove: 'hidden-right'
+    })
+    $("#contact").viewportChecker({
+        classToRemove: 'fade-in'
+    })
+    
+
+    // Scroll to the Top Button (show the button and its functionality)
+    $("#scrollToTop").click(e => {
+        $('html, body').animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+    window.onscroll = function() {scrollFunction()};
+   
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("scrollToTop").style.display = "block";
+        } else {
+            document.getElementById("scrollToTop").style.display = "none";
+        }
+    }
+
+    
+});
